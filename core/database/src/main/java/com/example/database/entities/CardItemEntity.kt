6a346a2c11f0f7694faa00ea934131384c9383ee
derrick <drocha616@gmail.com>
2026-07-27 21:51:15 -0600

    Added cardEntity.

--- core/database/src/main/java/com/example/database/entities/CardItemEntity.kt
+++ core/database/src/main/java/com/example/database/entities/CardItemEntity.kt
@@ -1,4 +1,14 @@
 package com.example.database.entities
 
-class CardItemEntity {
-}
+import androidx.room3.ColumnInfo
+import androidx.room3.Entity
+import androidx.room3.PrimaryKey
+
+@Entity(tableName = "card_item")
+data class CardItemEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    @ColumnInfo(name = "card_set") val cardSet: String,
+    @ColumnInfo(name = "grading_condition") val gradingCondition: String,
+    val valuation: Double
+)
